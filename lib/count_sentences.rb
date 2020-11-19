cfa45a2a3200498f0b516628e9ee68fb5b6ac73b
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-18 20:07:05 -0500

    sentences successfully counted

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #binding.pry
+    if self.end_with?(".")
+      return true
+    else 
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else 
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
-
+    splitted = self.split(/[\.?!]/)
+    count = splitted.select {|sent| sent.length > 0 }
+    count.length
   end
 end
